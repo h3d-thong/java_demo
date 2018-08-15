d3d486587683bb847f14554e9a317de4f843ff5e
ThongVX <vuongthong388@gmail.com>
2018-08-15 17:31:03 +0700

    ThongVX.Reactive-Programming : Thay đổi main

--- reactive_programming/src/main/java/ActiveProgramming.java
+++ reactive_programming/src/main/java/ActiveProgramming.java
@@ -1,9 +1,37 @@
 import io.reactivex.Flowable;
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 public class ActiveProgramming {
     public static void main(String[] args){
-        Flowable.just("Hello world").subscribe(System.out::println);
+        Observable<Integer> source = Observable.range(1,5);
+
+        Observer<Integer> consumer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println(d.toString());
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("innit "+integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Done");
+            }
+        };
+
+        source.subscribe(consumer);
 
     }
 }
